a4d624fb9a9a8dac81728dda10acd2c4d96a064e
DaikiAkimoto <akimoto@sonicjam.co.jp>
2018-06-08 11:28:37 +0900

    Initial commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Main : MonoBehaviour {
+    WebCamTexture webcamTexture;
+
+    GameObject planeObj;
+    Renderer renderer;
+
+    public int width = 1080;
+    public int height = 1920;
+    public int fps = 30;
+
+	// Use this for initialization
+	void Start () {
+        //
+        // Webカメラを検出
+        //
+        WebCamDevice[] devices = WebCamTexture.devices;
+        if (devices.Length > 0) {
+            //Debug.Log("Webカメラ検出");
+
+            webcamTexture = new WebCamTexture(width, height, fps);
+            planeObj = GameObject.Find("Plane");
+            renderer = planeObj.GetComponent<Renderer>();
+            renderer.material.mainTexture = webcamTexture;
+            webcamTexture.Play();
+
+        } else {
+            Debug.LogError("Webカメラが検出できません");
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/openingTitle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class openingTitle : MonoBehaviour {
+
+	Button startBtn;
+	GameObject parentObj;
+
+	// Use this for initialization
+	void Start () {
+		startBtn = GameObject.FindWithTag("startBtn").GetComponent<Button>();
+		startBtn.onClick.AddListener(startBtnClicked);
+	}
+
+	void startBtnClicked() {
+		SceneManager.LoadScene("Main");
+		// Debug.Log("btn clicked");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
